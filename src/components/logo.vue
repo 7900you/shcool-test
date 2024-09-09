3578e55ed8f6bc82b6a564c492a752e40a5056d3
xbin <bin22333@163.com>
2024-09-09 11:28:12 +0800

    ``` 集成ElementUI库并调整项目结构以符合规范
    项目中新增了ElementUI库，以改善UI组件并统一设计风格。删除了默认的导航栏和HelloWorld组件，以符合ElementUI的布局规范。同时，启用了history模式以支持美观的客户端路由。
    ```

---
+++ src/components/logo.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+</script>
+<template>
+   <div>
+    <div style="width: 100%; display: flex;height: 5rem; ">
+        <img src="../assets/logo.png" alt="logo" class="logo sty"></img>
+        
+    </div>
+    <div style="width: 100%; display: flex; height: 3rem;">
+        <div class="sty" style="color: aliceblue;">运维管理系统</div>
+    </div>  
+   </div> 
+</template>
+<style>
+.sty {  
+    margin: 1rem auto;
+    height: 5rem;
+    border-radius: 1rem;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,51 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div class="common-layout">
+        <el-container>
+            <el-aside width="200px">
+                <!-- 左侧菜单 -->
+                <el-menu router active-text-color="#409eff" background-color="#304156" class="el-menu-vertical-demo"
+                    :default-active="$route.path" text-color="#bfcbd9">
+                    <logo></logo>
+                    <!-- el-menu-item表示只有一级菜单 -->
+                    <el-menu-item index="/index">
+                        <el-icon>
+                            <HomeFilled />
+                        </el-icon>
+                        <span>首页</span>
+                    </el-menu-item>
+                    <el-menu-item index="/">
+                        <el-icon>
+                            <HomeFilled />
+                        </el-icon>
+                        <span>首页</span>
+                    </el-menu-item>
+
+
+                </el-menu>
+
+            </el-aside>
+            <el-main>
+                <!-- 页面内容 -->
+                <router-view></router-view>
+            </el-main>
+        </el-container>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import logo from '../components/logo.vue'
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+components: {
+    logo
+  },
 }
 </script>
+<style>
+body {
+    margin: 0;
+}
+.el-menu-vertical-demo {
+    /* vh表示视口高度 */
+    height: 100vh;
+}</style>
